9dc1796101c6a7267df4c4e23323f9201b804638
linganmin <saboran@163.com>
2019-07-26 23:58:06 +0800

    add travis ci

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -14,14 +14,18 @@ then
   
   ssh -t travis@47.96.70.2 -o StrictHostKeyChecking=no <<remotessh
   cd /data/wwwroot/
-  ls 
+
   if [ ! -d "/data/wwwroot/www.sosad.cn" ]; then
     mkdir www.sosad.cn
     chown -R www:www ./www.sosad.cn
   fi
+  
   exit;
 remotessh
 
-  rsync -az -vv --delete -e  public/ travis@47.96.70.2:/data/wwwroot/www.sosad.cn/
+ ls
+ pwd
+ hostname
+  # rsync -az -vv --delete -e  public/ travis@47.96.70.2:/data/wwwroot/www.sosad.cn/
 
 fi
