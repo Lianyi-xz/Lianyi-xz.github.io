fedc6d8486a10211290cbc0d02c69d541d76fd26
Lianyi_xz <zhaoran_xz@163.com>
2021-04-16 15:15:35 +0800

    Site updated: 2021-04-16 15:15:34

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Thu Jan 07 2021 09:43:54 GMT+0800 (GMT+08:00)
+// build time:Fri Apr 16 2021 15:15:29 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
